3191cbd1a1d05f318bf40105c122ce823988c451
Yeshwanth N <74974315+Yeshwanth-3085@users.noreply.github.com>
2022-10-30 11:33:30 +0530

    Update stack.h

--- stack.h
+++ stack.h
@@ -5,15 +5,27 @@
   Twitter UserName: Yeshwanth_3085
   LinkedIn Profile: Yeshwanth N
   Discord Profile: Yeshwanth N #4663
-  This header file can perform 8 Operations:
-  Creating a Stack,
-  Query of Capacity of the Stack,
-  Status of the Stack,
-  Push,
-  Pop,
-  Peek of the Stack,
-  Displaying all the elements of the Stack,
-  Flushing all the elements of the Stack.
+
+  This header file requires "stdio.h" & "stdlib.h" header files to be present in your compiler to work properly.
+
+  This header file can perform 9 Operations:
+
+  1) Creating a Stack { stack CreateStack(int Capacity); }, 
+  2) Query of Capacity of the Stack { int CapacityOfStack(stack Stack); },
+  3) Status of the Stack { void StatusOfStack(stack Stack); }, 
+  4) Push an element into the Stack { stack Push(stack Stack); }, 
+  5) Pop an element from the Stack { stack pop(stack Stack); }, 
+  6) Peek of the Stack { void Peek(stack Stack); }, 
+  7) Displaying all the elements of the Stack { void DisplayStack(stack Stack); },
+  8) Flushing all the elements of the Stack { stack FlushStack(stack Stack); }, 
+  9) Freeing the Stack { void FreeStack(stack Stack); }.
+
+  WARNING: Don't forget use the FreeStack() function at the end of your program, 
+           as this header file uses DMA (calloc()) method to allocate memory dynamically 
+           which needs to be freed after the execution to prevent memory leak.
+           To learn more search for Heap.
+
+  Thank you for using it 😊.
 */
 #include <stdio.h>
 #include <stdlib.h>
@@ -21,80 +33,85 @@ struct S
 {
   int Capacity, Top, *Arr;
 };
-struct S CreateStack(int Capacity)
+typedef struct S stack;
+stack CreateStack(int Capacity)
 {
-  struct S Stack;
+  stack Stack;
   Stack.Capacity = Capacity;
   Stack.Top = -1;
   Stack.Arr = (int *)calloc(Capacity, sizeof(int));
   return Stack;
 }
-int Capacity(struct S Stack)
+int CapacityOfStack(stack Stack)
 {
   return Stack.Capacity;
 }
-void Status(struct S Stack)
+void StatusOfStack(stack Stack)
 {
   if (Stack.Top < 0)
-    printf("The Stack is Empty.\n");
+     printf("The Stack is Empty.\n");
   else if (Stack.Top > Stack.Capacity - 2)
-    printf("The Stack is Full.\n");
-  else if (Stack.Top == 0)
-    printf("There is 1 element & %d spaces are left in the Stack.\n", Stack.Capacity - 1);
-  else if (Stack.Top == Stack.Capacity - 2)
-    printf("There are %d elements & 1 space is left in the Stack.\n", Stack.Top + 1);
-  else
-    printf("There are %d elements & %d spaces are left in the Stack.\n", Stack.Top + 1, Stack.Capacity - Stack.Top - 1);
+          printf("The Stack is Full.\n");
+       else if (Stack.Top == 0)
+               printf("There is 1 element & %d spaces are left in the Stack.\n", Stack.Capacity - 1);
+            else if (Stack.Top == Stack.Capacity - 2)
+                    printf("There are %d elements & 1 space is left in the Stack.\n", Stack.Top + 1);
+                 else
+                    printf("There are %d elements & %d spaces are left in the Stack.\n", Stack.Top + 1, Stack.Capacity - Stack.Top - 1);
 }
-struct S Push(struct S Stack)
+stack Push(stack Stack)
 {
   if (Stack.Top > Stack.Capacity - 2)
-    printf("Stack OverFlow!\n");
+     printf("Stack OverFlow!\n");
   else
-  {
-    printf("Enter an element to push into the stack: ");
-    scanf("%d", &Stack.Arr[++Stack.Top]);
-    printf("%d pushed into the Stack.\n", Stack.Arr[Stack.Top]);
-    return Stack;
-  }
+     {
+      printf("Enter an element to push into the stack: ");
+      scanf("%d", &Stack.Arr[++Stack.Top]);
+      printf("%d pushed into the Stack.\n", Stack.Arr[Stack.Top]);
+      return Stack;
+     }
 }
-struct S Pop(struct S Stack)
+stack Pop(stack Stack)
 {
   if (Stack.Top < 0)
-    printf("Stack UnderFlow!\n");
+     printf("Stack UnderFlow!\n");
   else
-  {
-    printf("%d poped from the Stack.\n", Stack.Arr[Stack.Top--]);
-    return Stack;
-  }
+     {
+      printf("%d poped from the Stack.\n", Stack.Arr[Stack.Top--]);
+      return Stack;
+     }
 }
-void Peek(struct S Stack)
+void Peek(stack Stack)
 {
   if (Stack.Top < 0)
-    printf("The Stack is Empty.\n");
+     printf("The Stack is Empty.\n");
   else
-    printf("The Top most element of the Stack is: %d.\n", Stack.Arr[Stack.Top]);
+     printf("The Top most element of the Stack is: %d.\n", Stack.Arr[Stack.Top]);
 }
-void Display(struct S Stack)
+void DisplayStack(stack Stack)
 {
   int i;
   if (Stack.Top < 0)
-    printf("The Stack is Empty.\n");
+     printf("The Stack is Empty.\n");
   else
-  {
-    printf("The Stack elements are:\n");
-    for (i = Stack.Top; i > -1; i--)
+     {
+      printf("The Stack elements are:\n");
+      for (i = Stack.Top; i > -1; i--)
       printf("%d\n", Stack.Arr[i]);
-  }
+     }
 }
-struct S Flush(struct S Stack)
+stack FlushStack(stack Stack)
 {
- if (Stack.Top < 0)
-    printf("Stack UnderFlow!\n");
- else
-    {
-     Stack.Top = -1;
-     printf("The Stack elements are Flushed.\n");
-     return Stack;
-    }
+  if (Stack.Top < 0)
+     printf("Stack UnderFlow!\n");
+  else
+     {
+      Stack.Top = -1;
+      printf("The Stack elements are Flushed.\n");
+      return Stack;
+     }
+}
+void FreeStack(stack Stack)
+{
+ free(Stack.Arr);
 }
